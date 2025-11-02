1de00cd398a729013ca7c027bbd9c7e55fe4c3e3
Cauê Castello Ferreira <cauecferreira@gmail.com>
2025-11-02 16:51:02 -0300

    added cors

--- fortserver/src/server.ts
+++ fortserver/src/server.ts
@@ -1,5 +1,24 @@
-import app from './app';
 import config from './config/config';
+import express from 'express';
+import cors from "cors";
+import itemRoutes from './routes/itemRoutes';
+import { errorHandler } from './middlewares/errorHandler';
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+app.use(cors(corsOptions));
+
+// Routes
+app.use('/api/items', itemRoutes);
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
