1bc74374b7b56e0755d23c5e0cb30a03f7e1f167
Cauê Castello Ferreira <cauecferreira@gmail.com>
2025-11-13 20:00:55 -0300

    inteface updates

---
+++ fortmarket/components/navbar.tsx
@@ -0,0 +1,72 @@
+import { cookies } from "next/headers";
+import NavigationMenuWithActiveItem from "./navigation-menu-04";
+import Link from "next/link";
+import { Archivo } from "next/font/google";
+import Image from "next/image";
+
+const archivo = Archivo({
+    subsets: ['latin'],
+})
+
+export default async function NavBar() {
+
+    const cookieList = await cookies();
+
+    const requestUser = await fetch('http://localhost:3030/credenciais/data',
+        {
+            method: 'POST',
+            headers: {
+                cookie: cookieList.toString(),
+            },
+            cache: "no-store",
+            credentials: "include",
+        }
+    )
+
+    let data = null;
+    if (requestUser.ok) {
+        data = await requestUser.json();
+    }
+
+    return (
+        <div className={archivo.className}>
+            <div className="p-4 flex flex-row
+            justify-between items-center-safe w-full
+            bg-zinc-50 dark:bg-[#101014]
+            ">
+                <div className="flex flex-row 
+                place-self-center-safe place-items-center-safe">
+                    <p className="text-xl p-2">FORTMARKET</p>
+                    <NavigationMenuWithActiveItem />
+                </div>
+                {data ?
+                    <div className="flex flex-row 
+                    place-items-center-safe
+                    ">
+                        <div className="flex flex-row 
+                        mx-2 my-4
+                        justify-between
+                        rounded-md
+                        bg-[#18181B] dark:bg-[#FFFFFF]
+                        text-[#FFFFFF] dark:text-[#18181B]
+                        ">
+                            <div className="relative size-6">
+                                <Image
+                                    src={"/Vbucks.svg"}
+                                    alt="vbuck icon"
+                                    layout="fill"
+                                    className="not-dark:invert"
+                                />
+                            </div>
+
+                            <p className="text-lg">{data.vbucks}</p>
+                        </div>
+                        <p className="self-center-safe">{data.nome}</p>
+                    </div> :
+                    <Link href={'/login'}>Login</Link>
+                }
+            </div>
+        </div>
+    )
+}
+
---
+++ fortmarket/components/navigation-menu-04.tsx
@@ -0,0 +1,40 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { BookOpen, Home, Rss } from "lucide-react";
+import Link from "next/link";
+
+const navigationMenuItems = [
+  { title: "Loja de items", href: "/", icon: Home, isActive: true },
+  { title: "Comunidade", href: "/comunidade", icon: Rss },
+];
+
+export default function NavigationMenuWithActiveItem({
+  className,
+}: React.ComponentProps<'div'>) {
+  return (
+    <NavigationMenu className={className}>
+      <NavigationMenuList>
+        {navigationMenuItems.map((item) => (
+          <NavigationMenuItem key={item.title}>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              asChild
+              active={item.isActive}
+            >
+              <Link href={item.href} 
+              className="flex-row items-center gap-2.5">
+                {/* <item.icon className="h-5 w-5 shrink-0" /> */}
+                {item.title}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
